bc37e170874bf55f69ccad105ec36b2bafd83f9a
jglim997 <jglim997@gmail.com>
2021-03-14 18:08:36 -0400

    second update

---
+++ homework_5/products.js
@@ -0,0 +1,15 @@
+// function newtrashtalk(){
+//     document.getElementById("img1").src = "images/trashtalk22.svg";
+//     // 204 IS ORIGINAL
+// }
+
+// function trashtalk(){
+//     document.getElementById("img1").src = "images/trashtalk.svg";
+
+function newblackberry(){
+    document.getElementById("img3").src = "images/newblackberry.png"; 
+}
+
+function blackberry(){
+    document.getElementById("img3").src = "images/blackberry.png";
+}
