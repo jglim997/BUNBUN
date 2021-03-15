5c4ab1424566bb1a4c26b1e15d40b39f202867c0
jglim997 <jglim997@gmail.com>
2021-03-14 22:17:04 -0400

    finished first three pages and reflection pdf

--- homework_5/products.js
+++ homework_5/products.js
@@ -1,11 +1,3 @@
-// function newtrashtalk(){
-//     document.getElementById("img1").src = "images/trashtalk22.svg";
-//     // 204 IS ORIGINAL
-// }
-
-// function trashtalk(){
-//     document.getElementById("img1").src = "images/trashtalk.svg";
-
 function newblackberry(){
     document.getElementById("img3").src = "images/newblackberry.png"; 
 }
