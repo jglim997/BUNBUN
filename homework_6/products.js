7175112c5cfe52ad2241cd06fdfff05c498f37d5
jglim997 <jglim997@gmail.com>
2021-04-02 15:56:40 -0400

    initial changes

---
+++ homework_6/products.js
@@ -0,0 +1,7 @@
+function newblackberry(){
+    document.getElementById("img3").src = "images/newblackberry.png"; 
+}
+
+function blackberry(){
+    document.getElementById("img3").src = "images/blackberry.png";
+}
