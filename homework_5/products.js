8dcde73d9770947212c10a36e4ebff03fc5ae885
jglim997 <jglim997@gmail.com>
2021-04-09 16:55:38 -0400

    I coded JS :)

--- homework_5/products.js
+++ homework_5/products.js
@@ -5,3 +5,18 @@ function newblackberry(){
 function blackberry(){
     document.getElementById("img3").src = "images/blackberry.png";
 }
+
+function cart1(){
+    var bun = document.getElementById("item1");
+    bun.remove();
+}
+
+function cart2(){
+    var bun = document.getElementById("item2");
+    bun.remove();
+}
+
+function cart3(){
+    var bun = document.getElementById("item3");
+    bun.remove();
+}
--- homework_6/products.js
+++ homework_6/products.js
@@ -5,3 +5,24 @@ function newblackberry(){
 function blackberry(){
     document.getElementById("img3").src = "images/blackberry.png";
 }
+
+function cart1(){
+    var bun = document.getElementById("item1");
+    bun.remove();
+    var x = document.getElementById("total");
+    x.innerHTML = "6 Items Total - $17.00";
+}
+
+function cart2(){
+    var bun = document.getElementById("item2");
+    bun.remove();
+    var x = document.getElementById("total");
+    x.innerHTML = "3 Items Total - $8.50";
+}
+
+function cart3(){
+    var bun = document.getElementById("item3");
+    bun.remove();
+    var x = document.getElementById("total");
+    x.innerHTML = "No Items in Cart"
+}
